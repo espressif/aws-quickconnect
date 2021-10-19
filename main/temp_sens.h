239d1446c794cd3c06f79bf0a78d380fa1f32925
Jason Carroll <czjaso@amazon.com>
2021-10-19 10:54:11 -0700

    Removed unnecessary files

--- main/temp_sens.h
+++
@@ -1,8 +0,0 @@
-#ifndef FMCONNECT_TEMP_SENS_H
-#define FMCONNECT_TEMP_SENS_H
-
-void tempSensStart(void);
-float getTempCelsius(void);
-uint32_t getRawTempReading(void);
-
-#endif /* FMCONNECT_TEMP_SENS_H */
